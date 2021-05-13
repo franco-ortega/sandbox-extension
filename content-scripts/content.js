ccd03923e51014e1d9c96959918843ddba52e379
Franco Ortega <francopdx@gmail.com>
2021-05-13 11:10:49 -0700

    initial commit

---
+++ content-scripts/content.js
@@ -0,0 +1,34 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    $("head").prepend(
+        `<style>
+            * {
+                transition-property: none !important;
+                transform: none !important;
+                animation: none !important;
+            }
+            img {
+                background-attachment: scroll;
+            }
+            img[src*=".gif"] {
+                opacity: 75%
+            }
+            img[alt*="GIF"] {
+                opacity: 25%
+            }
+          .slide-image {
+              height: auto;
+              width: 20vw;
+              position: fixed;
+              z-index: 2;
+          }
+        </style>`
+    );
+    $("body").prepend(
+        `<img  src="${request.url}" id="${request.imageDivId}"
+               class="slide-image" /> `
+    );
+    $(`#${request.imageDivId}`).click(function() {
+        $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
+    });
+    sendResponse({ fromcontent: "This message is from content.js" });
+});
